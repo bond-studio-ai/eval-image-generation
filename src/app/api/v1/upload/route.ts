6291075df9b301a6e5603672f9ea960a24bcdb88
Catriel Guillén <catriel@bondstudio.ai>
2026-03-14 16:54:20 -0300

    fix: Will this fix?

--- src/app/api/v1/upload/route.ts
+++ src/app/api/v1/upload/route.ts
@@ -9,6 +9,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
+  requestChecksumCalculation: 'WHEN_REQUIRED',
 });
 
 const BUCKET = process.env.AWS_S3_BUCKET!;
